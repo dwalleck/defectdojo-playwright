b7fbe21bb491fe63e71d36afa3e595fcc8252385
Daryl Walleck <dwalleck@gmail.com>
2023-09-26 15:26:22 -0500

    Added product and product type tests

--- dashboard_page.py
+++ dashboard_page.py
@@ -7,4 +7,5 @@ class DashboardPage:
         self.page = page
         self.add_product_link = page.get_by_role("link", name="Add Product", exact=True)
         self.all_products_link = page.get_by_role("link", name="All Products", exact=True)
+        self.add_project_type_link = page.get_by_role("link", name="Add Project Type", exact=True)
         self.projects_menu = page.get_by_label("Products")
--- test_add_product.py
+++ test_add_product.py
@@ -1,37 +1,34 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+import re
+from playwright.sync_api import Page, expect
+from login_page import LoginPage
+from dashboard_page import DashboardPage
+from add_project_page import AddProjectPage
 
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://demo.defectdojo.org/login?next=/")
-    page.get_by_label("Username*").click()
-    page.get_by_label("Username*").fill("admin")
-    page.get_by_label("Username*").press("Tab")
-    page.get_by_label("Password*").click()
-    page.get_by_label("Password*").fill("1Defectdojo@demo#appsec")
-    page.get_by_role("button", name="Login").click()
-    page.get_by_role("link", name="Add Product", exact=True).click()
-    page.get_by_label("Name*").fill("Another Product")
-    page.locator("pre").nth(1).click()
-    page.get_by_role("application").get_by_role("textbox").fill("Some details about another product")
-    page.get_by_placeholder("Enter some tags (comma separated, use enter to select / create a new tag)").click()
-    page.get_by_placeholder("Enter some tags (comma separated, use enter to select / create a new tag)").fill("stuff,")
-    page.locator("#base-content div").filter(has_text="Product manager --------- (user2) (admin) (product_manager) ---------").get_by_title("---------").click()
-    page.locator("#bs-select-1-2").click()
-    page.locator("#base-content div").filter(has_text="Technical contact --------- (user2) (admin) (product_manager) ---------").get_by_title("---------").click()
-    page.locator("#bs-select-2-2").click()
-    page.locator("#base-content div").filter(has_text="Product Type* --------- Billing Commerce Research and Development ---------").get_by_title("---------").click()
-    page.locator("#bs-select-4-1").click()
-    page.get_by_role("combobox", name="Default").click()
-    page.locator("#bs-select-5-0").click()
-    page.get_by_role("button", name="Submit").click()
-
-    # ---------------------
-    context.close()
-    browser.close()
+def test_add_new_product(page: Page):
+    login_page = LoginPage(page)
+    login_page.navigate()
+    login_page.login('admin', '1Defectdojo@demo#appsec')
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+    # Go to the add product page
+    dashboard_page = DashboardPage(page)
+    dashboard_page.projects_menu.hover()
+    dashboard_page.add_product_link.click()
+
+    # Fill out the details of the project and submit them
+    add_proj_page = AddProjectPage(page)
+    add_proj_page.name_field.fill('From Playwright4')
+    add_proj_page.description.fill('Something about Playwright')
+    add_proj_page.project_manager.select_option('(admin)')
+    add_proj_page.project_type.select_option('Billing')
+    add_proj_page.sla_config.select_option('Default')
+    add_proj_page.submit_button.click()
+
+    # Verify the new product is listed
+    dashboard_page.projects_menu.hover()
+    dashboard_page.all_products_link.click()
+    pw_cell = page.locator('tr').filter(has_text='From Playwright4')
+    expect(pw_cell).to_be_visible()
+
+    
